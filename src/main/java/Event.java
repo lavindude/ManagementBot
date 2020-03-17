7238bdf9a2b7fa44497b9c4712ae748d75e9b54b
Lavindu <lavindude@gmail.com>
2020-03-17 08:49:39 -0700

    First commit

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
-
 public class Event {
+    private String s;
+    private String month;
+    private int day;
+    private int year;
+    private int hour;
+    private int minute;
+
+    public Event(String s, String month, int day, int year, int hour, int minute) {
+        this.s = s;
+        this.month = month;
+        this.day = day;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+
+        //throw exception for minutes?
+    }
+
+    public Event() {}
+
+    public String toString() {
+        return (this.s + " is due: " + this.month + " " + this.day + "th, " + this.year + " at " + this.hour + this.minute);
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public void setMonth(String m) {
+        this.month = m;
+    }
+
+    public void setDay(int d) {
+        this.day = d;
+    }
+
+    public void setYear(int y) {
+        this.year = y;
+    }
+
+    public void setHour(int h) {
+        this.hour = h;
+    }
+
+    public void setMinute(int m) {
+        this.minute = m;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
 
 public class Main extends ListenerAdapter {
+    private ArrayList<Event> events = new ArrayList<Event>();
+
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         String token = "Njg4MTAyMzA1NjI2NTg3MTgy.XmvciQ.LwTLgmnkWZGdO1KDWTksRGCBGrI";
@@ -16,19 +19,129 @@ public class Main extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        System.out.println("We recieved a message from " +
+        System.out.println("We received a message from " +
                 event.getAuthor().getName() + " \"" +
                 event.getMessage().getContentDisplay() + "\"   ------     channel: " + event.getChannel());
+
         String mes = event.getMessage().getContentRaw();
 
-        if (mes.equals("!test1")) {
-            event.getChannel().sendMessage("Welcome to this server!").queue();
+        int sub1 = mes.indexOf("!addEvent");
+        int sub2 = mes.indexOf("!setMonth");
+        int sub3 = mes.indexOf("!setDay");
+        int sub4 = mes.indexOf("!setYear");
+        int sub5 = mes.indexOf("!addEventManually");
+        int sub6 = mes.toLowerCase().indexOf("fuck");
+        int sub7 = mes.toLowerCase().indexOf("shit");
+        int sub8 = mes.toLowerCase().indexOf("pussy");
+        int sub9 = mes.toLowerCase().indexOf("ass");
+        int sub10 = mes.toLowerCase().indexOf("gay");
+        int sub11 = mes.toLowerCase().indexOf("jew");
+        int sub12 = mes.toLowerCase().indexOf("cum");
+        int sub13 = mes.toLowerCase().indexOf("penis");
+        int sub14 = mes.toLowerCase().indexOf("vagina");
+
+        if (sub1 != -1) {
+            Event e = new Event();
+            events.add(e);
+            Event edit = events.get(events.size() - 1);
+            edit.setS(mes.substring(10));
+            event.getChannel().sendMessage("Event successfully added!").queue();
         }
 
-        else if (mes.equals("!test21")) {
-            event.getChannel().sendMessage("Pong!").queue();
+        else if (sub2 != -1) {
+            Event edit = events.get(events.size() - 1);
+            edit.setMonth(mes.substring(10));
+            event.getChannel().sendMessage("Month of the event successfully set!").queue();
         }
 
+        else if (sub3 != -1) {
+            event.getChannel().sendMessage("What do you want the day of the event's deadline to be?").queue();
+            Event edit = events.get(events.size() - 1);
+            int m = Integer.parseInt(mes.substring(8));
+            edit.setDay(m);
+            event.getChannel().sendMessage("Day of the event successfully set!").queue();
+        }
 
+        else if (sub4 != -1) {
+            Event edit = events.get(events.size() - 1);
+            int m = Integer.parseInt(mes.substring(9));
+            edit.setYear(m);
+            event.getChannel().sendMessage("Year of the event successfully set!").queue();
+        }
+
+        else if (sub5 != -1) {
+            addEventManually(mes);
+        }
+
+        //error with indexes maybe
+        else if (sub6 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub7 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub8 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub9 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub10 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub11 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub12 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub13 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (sub14 != -1) {
+            event.getChannel().sendMessage("No swearing!").queue();
+            event.getMessage().delete().queue();
+        }
+
+        else if (mes.equals("!print")) {
+            for (Event e : events) {
+                String p = e.toString();
+                event.getChannel().sendMessage(p).queue();
+            }
+        }
     }
+
+    public void addEventManually(String mes) {
+        //check for logic errors, there is a logic error with the cursor variable, need to fix
+        int cursor = 18;
+        String rest1 = mes.substring(cursor);
+        int space1 = rest1.indexOf(" ");
+        String sendMessage = mes.substring(18, space1);
+        //code not finished: !addEventmanually eat March 6, 2020 13:49
+        cursor = space1 + 1;
+        String rest2 = mes.substring(cursor);
+        int space2 = rest2.indexOf(" ");
+        String sendMonth = rest2.substring(cursor, space2);
+        cursor = space2 + 1;
+        int comma = rest2.indexOf(",");
+        int sendDay = Integer.parseInt(mes.substring(space2, comma));
+    }
+
+
 }
